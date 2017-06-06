ff40a71dd929ec294ec3810e4caf90bd3e956638
LukAssArts <lukas.biermann@googlemail.com>
2017-06-06 20:18:22 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+echo '<ul>
+	  <li><a href="index.php">Startseite</a></li>
+	  <li class="dropdown">
+		<a href="books.php" class="dropbtn">B&uuml;cher</a>
+		<div class="dropdown-content">
+		  <a href="books.php#krimis">Krimis</a>
+		  <a href="books.php#romane">Romane</a>
+		  <a href="books.php#fantasy">Fantasy</a>
+		</div>
+	  </li>
+	   <li><a href="contact.php">Kontakt</a></li>
+	</ul>';
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html5>
+<html>
+<head>
+  <title>Buchhandel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="theme.css">
+  <!--<script src="myCode.js"></script>-->
+</head>
+<body>
+	<!--header:-->
+	<?php 
+		include 'header.php';
+	?>
+	
+	<!--content:-->
+	<div class="content">
+		<h1 class="header">
+			header h1
+		</h1>
+		<p>
+			paragraph 1 <br>
+			some text....
+		</p>
+		<h2 class="header">
+			header h2
+		</h2>
+		<p>
+			paragraph 2 <br>
+			some text....
+		</p>
+	</div>
+</body>
+</html>
