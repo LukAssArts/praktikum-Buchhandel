8aaeb9a719bd22b3bc815d14a2ab98765c9b7f83
Sven Fiergolla <fiergoll@infcip54.stud.informatik.uni-trier.de>
2017-06-08 15:43:09 +0200

    changed kontakformular

---
+++ Kontaktfeld.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+	<title>Kontaktformular</title>
+
+</head>
+
+<body>
+<?php
+
+$nameErr = $emailErr = $nachrichtErr = "";
+$name = $email = $nachricht = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name wird benötigt";
+  } else {
+    $name = test_input($_POST["name"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Nur Buchstaben und Leertaste!"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email wird benötigt";
+  } else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Ungültiges Email-format"; 
+    }
+  }
+  
+  if (empty($_POST["nachricht"])) {
+	$adrErr = "Nachricht leer";
+ } 
+ 
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+
+<h2>Kontaktformular</h2>
+<p><span>* Benötigte Angaben</span></p>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+<table>
+<tr>
+	<td>Name:</td>
+	<td><input type="text" name="name" value="<?php echo $name;?>"></td>
+	<td> <span>* <?php echo $nameErr;?></span></td>
+</tr>
+<tr>
+	<td>E-mail:</td>
+	<td><input type="text" name="email" value="<?php echo $email;?>"></td>
+	<td><span>* <?php echo $emailErr;?></span></td>
+</tr>
+<tr>
+	<td>Nachricht:</td>
+	<td><textarea type="text" name="nachricht" rows="5" cols="40" value="<?php echo $nachricht;?>"></textarea></td>
+	<td><span>* <?php echo $nachrichtErr;?></span></td>
+</tr>
+
+</table>   
+
+<input type="submit" name="submit" value="Senden">  
+</form>
+</body>
+
+</html>
